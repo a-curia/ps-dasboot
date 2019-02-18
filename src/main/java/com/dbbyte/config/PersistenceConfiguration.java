bb337b6a62c6198282d3a7c2801d05bfc61a58c0
Gheorghe-Adrian Curia <gheorgheadrian.curia@ihsmarkit.com>
2019-02-18 12:40:41 +0200

    Defining Multiple Datasources

---
+++ src/main/java/com/dbbyte/config/PersistenceConfiguration.java
@@ -0,0 +1,30 @@
+package com.dbbyte.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class PersistenceConfiguration {
+
+	@Bean
+	@ConfigurationProperties(prefix="spring.datasource")
+	@Primary
+	public DataSource dataSource() {
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAA");
+		return DataSourceBuilder.create().build();
+	}
+	
+	@Bean
+	@ConfigurationProperties(prefix="datasource.flyway")
+	@FlywayDataSource
+	public DataSource flywayDataSource() {
+		System.out.println("BBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+		return DataSourceBuilder.create().build();
+	}
+}
