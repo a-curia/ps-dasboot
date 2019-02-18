4ea9d14773aa7e280980d5bd750bb4a7b1f9933c
Gheorghe-Adrian Curia <gheorgheadrian.curia@ihsmarkit.com>
2019-02-18 11:13:41 +0200

    rest api with stub

---
+++ src/main/java/com/dbbyte/PsDasbootApplication.java
@@ -0,0 +1,14 @@
+package com.dbbyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsDasbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsDasbootApplication.class, args);
+	}
+
+}
+
